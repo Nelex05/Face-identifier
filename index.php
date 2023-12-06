c12cfc25236206e155b05b6fe9bf393bd727db2e
Nelex05 <151332284+Nelex05@users.noreply.github.com>
2023-12-06 11:26:11 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Face_identifier</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel = "stylesheet" type = "text/css" href = "style.css" />
+</head>
+<body>
+  <div class="card-body" id="body">
+      <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-8" id="col">
+          <nav class="navbar navbar-expand-md navbar-dark">
+              <a class="navbar-brand" href="index.php" id="Face_Idetifier">Face Idetifier</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar" style="float: left;">
+                  <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                      <a class="nav-link" id="a1" href="index.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" id="a1" href="index.php">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" id="a1" href="index.php">Tutorial</a>
+                    </li>    
+                  </ul>
+              </div>  
+          </nav>
+        </div>
+        <div class="col-md-1">
+        </div>
+      </div><br>
+      <div class="row">
+        <center><img src="eye.gif" alt="">
+        <img src="eye.gif" alt=""></center>
+      </div>
+      
+      <br>
+      <div class="row" >
+        <div class="col-md-4">
+            <center><h2>this is right</h2></center>
+        </div>
+        <div class="col-md-4">
+            <center>
+              <h2>we have to identify you first</h2>
+            </center>
+            <br><br>
+            <center>
+              <divs class="col-md-10">
+                <center>
+                  <p>War has been a constant presence throughout human history, leaving permanent impacts on communities, civilizations, and individuals alike. It has stirred a plethora of emotions, stimulated philosophical discussions, and offered fertile ground for contemplation as one of the most profound and momentous human experiences. Essays on war, as windows into the human condition, provide a chance to investigate the many facets of armed conflict.
+
+                    The subject of war has continually altered, from ancient battles engraved on stone tablets to modern violence witnessed through the prism of technology, yet its essential essence remains immovable. Essays on war investigate the causes that drive nations and people to engage in violence, revealing the complex interplay of political, economic, social, and ideological variables that culminate in these cataclysmic events.
+                  </p>
+                </center>  
+              </divs>
+            </center>
+            
+        </div>
+        <div class="col-md-4">
+            <center><h2>this is left</h2></center>
+        </div>
+      </div>
+      <br><br><br><br><br><br><br>
+    </div>
+    <center><h1>violence witnessed through the prism of technology, yet its essential essence remains immovable. Essays on war investigate the causes that drive nations and people to engage in violence, revealing the complex interplay of political, economic, social, and ideological variables that culminate in these cataclysmic events.</h1></center>
+</body>
+</html>
