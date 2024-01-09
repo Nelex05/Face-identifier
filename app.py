9e56383c02736129ef6343c78c6f5d877f5f5e77
Nelex05 <151332284+Nelex05@users.noreply.github.com>
2024-01-09 09:10:22 +0800

    i add for the camera

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import pathlib
 import cv2
+
+cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
+
+clf = cv2.CascadeClassifier(str(cascade_path))
+
+camera = cv2.VideoCapture(0)
