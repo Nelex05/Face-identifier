17ba508492837da50512bc89d1fc77ed9eb81077
Nelex05 <151332284+Nelex05@users.noreply.github.com>
2024-01-14 17:26:01 +0800

    this is just a structure of my registration

---
+++ register/register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Face_identifier</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel = "stylesheet" type = "text/css" href = "Style.css" />
+</head>
+<body>
+    <br>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4">
+                <h4 id="h4">War has been a constant presence throughout human history, leaving permanent impacts on communities, civilizations, and individuals alike. It has stirred a plethora of emotions, stimulated philosophical discussions, and offered fertile ground for contemplation as one of the most profound and momentous human experiences. Essays on war, as windows into the human condition, provide a chance to investigate the many facets of armed conflict.
+                    The subject of war has continually altered. The subject of war has continually altered.. The subject of war has continually altered and individuals alike. It has stirred a plethora of emotions, stimulated philosophical discussions, and offered fertile ground
+                </h4>
+            </div>
+            <div class="col-md-4">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
