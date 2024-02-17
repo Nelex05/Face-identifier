a575ed9442c03b0eac6d7b341fd9b7c6784ee595
Nelex05 <151332284+Nelex05@users.noreply.github.com>
2024-02-17 15:44:54 +0800

    i added here

--- register/register.php
+++ register/register.php
@@ -9,7 +9,7 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel = "stylesheet" type = "text/css" href = "style.css" />
+  <link rel = "stylesheet" type = "text/css" href = "Style.css" />
 </head>
 <Style>
     #pwd{
@@ -83,10 +83,12 @@ a:link, a:visited {
                                     <div class="btn-group" >
                                         <div class="col-md-1" style="margin-left: -9px;">
                                             <button class="btn1" type="submit" class="btn btn-primary">
-                                                <span class="circle">
-                                                    <span class="arrow"></span>
-                                                </span>
-                                                <span class="text">Next</span>
+                                                    <a class="circle" href="face_registration.php">
+                                                        <span class="arrow"></span>
+                                                    </a>
+                                                    <a href="face_registration.php" class="text">
+                                                        <span class="text">Next</span>
+                                                    </a>
                                             </button>
                                         </div>
                                     </div>
@@ -105,50 +107,6 @@ a:link, a:visited {
                         </form>
                     </div>
                 </div>
-                <div class="card-body" style="background:rgba(247, 247, 247, 0.2); object-fit: cover;">
-                    <div class="warp" style="object-fit: cover;">
-                        <p class="text-center" style="font-size:50px;color:rgb(255, 255, 255); margin-left: 1px;">Sign up</p>
-                        <form name="myForm" action="register.php" onsubmit="return validateForm()" method="post" required>
-                            <br>
-                                <center>
-                                    <video id="videoCam" style="width: 83%"></video>
-                                    <script>
-                                        window.onload = function(){
-                                        let All_mediaDevices=navigator.mediaDevices
-                                            if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
-                                                console.log("getUserMedia() not supported.");
-                                                return;
-                                            }
-                                            All_mediaDevices.getUserMedia({
-                                                audio: true,
-                                                video: true
-                                            })
-                                            .then(function(vidStream) {
-                                                var video = document.getElementById('videoCam');
-                                                if ("srcObject" in video) {
-                                                    video.srcObject = vidStream;
-                                                } else {
-                                                    video.src = window.URL.createObjectURL(vidStream);
-                                                }
-                                                video.onloadedmetadata = function(e) {
-                                                    video.play();
-                                                };
-                                            })
-                                            .catch(function(e) {
-                                                console.log(e.name + ": " + e.message);
-                                            });
-                                            var x = document.getElementById("btn3");
-
-                                            if(x.style.display === "none"){
-                                                x.style.display = "block";
-                                            }
-                                            else{
-                                                x.style.display = "none";
-                                            }
-                                        }
-                                    </script>
-                                </center>
-            </div>
             <div class="col-md-4">
 
             </div>
