a0ee0f9284159bab4a56e3cef2731f4bc614c036
Nelex05 <151332284+Nelex05@users.noreply.github.com>
2024-03-10 13:45:28 +0800

    i change the css name

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel = "stylesheet" type = "text/css" href = "style1.css" />
+  <link rel = "stylesheet" type = "text/css" href = "Style1.css" />
 </head>
 <style>
   .btn1{
