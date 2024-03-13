332b4a707a10cce61cad7d94dd5335608f4f3b78
Nelex05 <151332284+Nelex05@users.noreply.github.com>
2024-03-13 13:06:18 +0800

    i added new here for datebase

--- register/registerdata.php
+++ register/registerdata.php
@@ -1,5 +1,5 @@
 <?php
-include("../data/db_con.php");
+include("../register/data/db_con.php");
 $username = $_POST['username'];
 $password = $_POST['password'];
 $sql = "INSERT INTO user (username,password)  VALUES ('$username','$password')"; 
@@ -7,7 +7,7 @@ $sql = "INSERT INTO user (username,password)  VALUES ('$username','$password')";
 $result = $conn->query($sql);
 
     if($result){
-        header("location:index.php/../../..");       
+        header("location:../register/face_reg.php");       
         }
     else{
         echo "<script>alert('naay kulang choi!!');window.location.replace('register/register.php');</script>";
